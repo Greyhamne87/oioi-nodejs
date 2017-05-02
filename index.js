81e12f7bba5c056eea1aa1c18b2d183ce8632d16
Grahamne Muir <grahamne@bluesquareoffices.com>
2017-05-02 17:07:57 +0100

    user listing get request

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var Listing = require('./models/listing');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
-var LocalStrategy    = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 
-  
+
 var app = express();
 var port = process.env.PORT || 3000
 
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true); 
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
@@ -46,31 +46,31 @@ app.use(function(req, res, next) {
 //Strats
 
 // Non auth
-app.post('/user/login', jsonParser, function(req, res, next){
+app.post('/user/login', jsonParser, function(req, res, next) {
 
-    User.findOne({email: req.body.email},function(err, user){
-        if (err){
+    User.findOne({ email: req.body.email }, function(err, user) {
+        if (err) {
             return res.status(500).json({
                 title: 'An error occured',
-                error:err
+                error: err
             });
         }
 
-        if(!user){
+        if (!user) {
             return res.status(401).json({
                 title: 'Invalid login credentials',
-                error:err
+                error: err
             });
         }
 
-        if (!bcrypt.compareSync(req.body.password, user.password)){
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
             return res.status(401).json({
                 title: 'Invalid login credentials',
-                error:err
+                error: err
             });
         }
 
-        var token = jwt.sign({user: user}, secret, {expiresIn:7200});
+        var token = jwt.sign({ user: user }, secret, { expiresIn: 7200 });
         res.status(200).json({
             message: 'Logged in',
             token: token,
@@ -92,11 +92,11 @@ app.post('/user/register', jsonParser, function(req, res, next) {
     });
 
     // Tell mongoose to save this to the database
-    user.save(function(err,result){
-        if (err){
+    user.save(function(err, result) {
+        if (err) {
             return res.status(500).json({
                 title: 'An error occured',
-                error:err
+                error: err
             });
         }
 
@@ -107,54 +107,120 @@ app.post('/user/register', jsonParser, function(req, res, next) {
     });
 });
 
+/** User requesting a passoword reset  **/
+// We need to add the logic this is examples sake
+app.post('/user/forgotPassword', jsonParser, function(req, res, next) {
 
-/** User adding a listing **/
-app.post('/user/listItem', jsonParser, function(req, res, next) {
-
-
-
-    var title           = req.body.title;
-    var subtitle        = req.body.subtitle;
-    var category        = req.body.category;
-    var description     = req.body.description;
-    var cost            = req.body.cost;
-    var postage         = req.body.postage;
-    var offerPostage    = req.body.offerPostage;
-    var orNearestOffer  = req.body.orNearestOffer;
-    var ownerId          = req.body.userId;
-
-    
-
-    // An object based on the regiser model
-    var listing = new Listing({
-        title: title,
-        subtitle: subtitle,
-        category: category,
-        description: description,
-        cost: cost,
-        postage: postage,
-        offerPostage: offerPostage,
-        ono: orNearestOffer,
-        ownerId: ownerId,
-
-    });
-
-    // Tell mongoose to save this to the database
-    listing.save(function(err,result){
-        if (err){
+    User.findOne({ email: req.body.email }, function(err, user) {
+        if (err) {
             return res.status(500).json({
                 title: 'An error occured',
-                error:err
+                error: err
+            });
+        }
+
+        if (!user) {
+            return res.status(401).json({
+                title: 'Invalid login credentials',
+                error: err
             });
         }
 
         res.status(201).json({
-            message: 'Saved the new user',
-            obj: result
+            message: 'Trigger password reset here',
+            userId: user._id
         });
+
     });
 });
 
+
+/** User adding a listing **/
+app.post('/user/listItem', jsonParser, function(req, res, next) {
+
+    console.log(req.body.userId);
+
+    var decoded = jwt.decode(req.body.userId);
+    var title = req.body.title;
+    var subtitle = req.body.subtitle;
+    var category = req.body.category;
+    var description = req.body.description;
+    var cost = req.body.cost;
+    var postage = req.body.postage;
+    var offerPostage = req.body.offerPostage;
+    var orNearestOffer = req.body.orNearestOffer;
+
+
+    User.findById(req.body.userId, function(err, user) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+
+
+        //
+        // An object based on the regiser model
+        var listing = new Listing({
+            title: title,
+            subtitle: subtitle,
+            category: category,
+            description: description,
+            cost: cost,
+            postage: postage,
+            offerPostage: offerPostage,
+            ono: orNearestOffer,
+            user: user
+        });
+
+        // Tell mongoose to save this to the database
+        listing.save(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occured',
+                    error: err
+                });
+            }
+
+            res.status(201).json({
+                message: 'Saved the new listing',
+                obj: result
+            });
+        });
+        //
+    });
+});
+
+
+/** Grabbing a users listings **/
+app.get('/user/myItems/userId/:id/token/:token', jsonParser, function(req, res, next) {
+
+    User.findById(req.params.id, function(err, user) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            });
+        }
+
+        Listing.find({ user: user }, function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occured',
+                    error: err
+                });
+            }
+
+            res.status(201).json({
+                message: 'grabbed the listings',
+                obj: result
+            });
+        });
+    });
+
+
+});
 
 
 /*
